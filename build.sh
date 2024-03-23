26c5737497ea43cea3c1529589964e2421ad992a
Vlad Patryshev <vpatryshev@gmail.com>
2024-03-23 13:24:46 +0100

    fixed the build script

--- build.sh
+++ build.sh
@@ -2,9 +2,18 @@
 
 # Use this script to build a pdf from a tex file.
 
-[   -z $1 ] && echo "need file path" && exit 1
-[ ! -r $1 ] && echo "Cant read $1"   && exit 1
-filename=`basename $1`
+[   -z "$1" ] && echo "need file path" && exit 1
+[ ! -r "$1" ] && echo "Cant read $1"   && exit 1
+filename=$(basename "$1")
 name="${filename%.*}"
-echo ^Z | pdflatex $1 >/dev/null && open $name.pdf || echo "Failed!\npdflatex $1" && cat $name.log && exit 1
-rm -f $name.aux; rm -f $name.log
+log="$name.log"
+
+if ! echo ^Z | pdflatex "$1" >/dev/null; then
+  printf 'Failed! See the log %s' "$log"
+  exit 1
+fi
+
+rm -f "$name.aux"
+rm -f "$log"
+open "$name.pdf"
+
